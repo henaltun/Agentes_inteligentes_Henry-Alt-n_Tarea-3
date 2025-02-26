993977bb4104602cff266433623be4b30baefac2
henaltun <estuardohaltun2394@gmail.com>
2025-02-26 00:04:19 -0600

    Update Agente_Patrullaje.py

--- Agente_Patrullaje.py
+++ Agente_Patrullaje.py
@@ -38,9 +38,3 @@ ruta_patrullaje = ["Punto A", "Punto B", "Punto C", "Punto D", "Punto E"]
 agente = Patrullero(ruta_patrullaje)
 agente.patrullar()
 
-# Definir una ruta de patrullaje
-ruta_patrullaje = ["Punto A", "Punto B", "Punto C", "Punto D", "Punto E"]
-
-# Crear el agente y ejecutarlo
-agente = Patrullero(ruta_patrullaje)
-agente.patrullar()
